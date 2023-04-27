067ff350f352893882cecf517b0ffe99335a2ebc
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-04-27 11:03:35 -0400

    Revert "login in and logout functional, changed Link for Task Tracker in the …"

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import Header from "./components/Header";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import PlayGame from "./pages/PlayGame";
-import { useState } from 'react'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState('');
-  const sendRequest = (str) => { setUser(str); }
-
+  const [username, setUsername] = useState('');
+  const getUsername = (e) => {
+    setUsername(e);
+  }
   //game needs to generate a link instead of going to /game
   return (
     <>
       <Router>
         <div className='container'>
-        <Header user={user} />
+          <Header username={username} />
           <Routes>
-          <Route path='/' element={<Login sendRequest={sendRequest} />} />
-          <Route path='/home' element={<Home user={user} />} />
-            <Route path='/playgame' element={<PlayGame user={user}/>} />
-
+            <Route path='/' element={<Login username={(e) => getUsername(e)} />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/playgame' element={<PlayGame />} />
           </Routes>
           <ToastContainer />
         </div>
